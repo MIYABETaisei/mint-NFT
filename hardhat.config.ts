5b6c2d3cda146243d590ee5b55ec92eaa4492b39
MIYABETaisei <mytspe46@gmail.com>
2022-07-25 23:37:12 +0900

    ✨ 環境変数の追加

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_KINKEBY_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    }
+  }
 };
 
 export default config;
