4f4ec53e79da850f323857acc1904a1b223e6900
MIYABETaisei <mytspe46@gmail.com>
2022-07-26 08:04:13 +0900

    ✨ add networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,19 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config()
 
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 
+const networks: { [key: string]: any } = {
+  rinkeby: {
+    url: process.env.ALCHEMY_RINKEBY_URL,
+    accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+  },
+};
+
 const config: HardhatUserConfig = {
+  defaultNetwork: "rinkeby",
+  networks,
   solidity: "0.8.9",
-  networks: {
-    rinkeby: {
-      url: process.env.ALCHEMY_KINKEBY_URL,
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
-    }
-  }
 };
 
 export default config;
